f6580e4f409fad376ae1173b07776523e304e7b1
Daniel Hagos <danielwukro@gmail.com>
2024-03-15 11:58:43 -0700

    Initial commit

---
+++ src/CoreJava.java
@@ -0,0 +1,29 @@
+public class CoreJava {
+
+
+
+        public static void main(String[] args) {
+
+            int num1 = 50;
+            int num2 = 70;
+
+
+            int sum = num1 + num2;
+
+
+            System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
